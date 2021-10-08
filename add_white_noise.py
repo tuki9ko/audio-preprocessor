171ce3e3f335fb4cc222d79eb3c887f1382a7022
tuki9ko <arkmisha@gmail.com>
2021-10-08 11:18:19 +0900

    update white noise source

--- add_white_noise.py
+++ add_white_noise.py
@@ -1,5 +1,24 @@
 import sys
 import numpy as np
+import librosa
+import soundfile as sf
+
+def add_white_noise(x, rate=0.002):
+	return x + rate*np.random.randn(len(x))
 
 if __name__ == '__main__':
-    
+	args = sys.argv
+
+	if len(args) >= 2:
+		file_name = args[1]
+	else:
+		print("usage: python add_white_noise.py [file_name]")
+		exit
+	
+	y, sr = librosa.load(file_name)
+
+	z = add_white_noise(y)
+
+	sf.write("white_" + file_name, z, sr)
+
+	
